cc2588aa3c39ecb0f6f829a7aec8c90d16d18604
Jepwyy <107722564+Jepwyy@users.noreply.github.com>
2022-12-19 03:19:37 +0800

    first commit

---
+++ Triangle Checker/Triangle Checker/Form1.vb
@@ -0,0 +1,20 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim firstAngle, secondAngle, thirdAngle, Sum As Integer
+        Try
+            firstAngle = Convert.ToInt32(TextBox1.Text)
+            secondAngle = Convert.ToInt32(TextBox2.Text)
+            thirdAngle = Convert.ToInt32(TextBox3.Text)
+            Sum = firstAngle + secondAngle + thirdAngle
+            If Sum = 180 Then
+                TextBox4.Text = "180 degrees.That's a GOOD Triangle!"
+            Else
+                TextBox4.Text = Sum.ToString() + " degrees. That's a BAD Triangle!"
+            End If
+        Catch ex As Exception
+            MessageBox.Show("Please input a number.", "Invvalid Input",
+                            MessageBoxButtons.OK, MessageBoxIcon.Warning)
+        End Try
+    End Sub
+End Class
